09f7a213621bc40e2d4c959ec9411601f4a78a84
Joseph Attia <josephattia159@gmail.com>
2021-08-06 13:40:51 -0400

    Created Arrayist of Clients

--- src/Server/ChatServer.java
+++ src/Server/ChatServer.java
@@ -1,15 +1,23 @@
 package Server;
 
+import Client.Client;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ChatServer {
 
     public static void acceptClients(ServerSocket serverS){
+        ArrayList<ClientTread> clients = new ArrayList<ClientTread>();
         while (true){
             try {
                 Socket socket = serverS.accept();
+                ClientTread client = new ClientTread(socket);
+                Thread thread = new Thread(client);
+                thread.start();
+                clients.add(client);
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/Server/ClientTread.java
+++ src/Server/ClientTread.java
@@ -1,4 +1,17 @@
 package Server;
 
-public class ClientTread {
+import java.net.Socket;
+
+public class ClientTread extends ChatServer implements Runnable{
+
+    private Socket socket;
+
+    public ClientTread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+
+    }
 }
